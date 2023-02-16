dde476b780753349573a23b18d3a2476a1015f37
Francis <fn87@drexel.edu>
2023-02-16 00:47:17 -0500

    New stuff before Sprint Review

--- workspace/forms.py
+++ workspace/forms.py
@@ -2,8 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
 class RegisterForm(FlaskForm):
+    firstName = StringField(label='First Name:')
+    lastName = StringField(label='Last Name:')
     username = StringField(label='Username:')
     password = PasswordField(label='Password:')
     passwordConfirm = PasswordField(label='Confirm Password:')
     email = StringField(label='Email Address:')
+    age = StringField(label='Age')
+    postalCode = StringField(label='Zip Code')
     submit = SubmitField(label='Create Account!')
