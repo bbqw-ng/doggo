a5a7d9d81212ce953ad55b65f84e553fe59c1a29
aw3477 <aw3477@drexel.edu>
2023-02-27 21:19:33 -0500

    homepage v2

--- workspace/routes.py
+++ workspace/routes.py
@@ -17,7 +17,7 @@ class sqlhost():
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return render_template('home.html')
+    return render_template('under_construction.html')
 
 @app.route("/register", methods=['GET', 'POST'])
 def register_page():
