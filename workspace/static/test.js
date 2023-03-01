40fffb14c17754e68a2dffdf7180134b2cb4903c
Brian Wang <bw672@drexel.edu>
2023-02-28 20:14:48 -0500

    listings site change to bolding when clicked; fixed image issues, added post button( no functionality yet)

--- workspace/static/test.js
+++ workspace/static/test.js
@@ -1,9 +1,11 @@
 const filterbuttons = document.querySelectorAll(".filterbutton");
-
 function bold(e){
-    e.classList.add("bold")
+    filterbuttons.forEach(element=>{
+        element.classList.remove("bold");
+    })
+    this.classList.add("bold");
 }
 
 filterbuttons.forEach(element => {
-    element.addEventListener("onclick",bold)
+    element.addEventListener("click",bold)
 });
