1bdead9e005d5f27652f03423dec8deff2d59372
Francis <fn87@drexel.edu>
2023-05-20 17:21:05 -0400

    implemented animations to ratings

--- workspace/static/jsfiles/stars.js
+++ workspace/static/jsfiles/stars.js
@@ -6,18 +6,51 @@ stars.forEach((star, index1) => {
 
     // Add a click event listener to each star
     star.addEventListener('click', () => {
+        updateRating(index1 + 1);
+        updateGoldStars(index1);
+    });
 
-        // Iterate over each star again to determine the rating and apply color
-        stars.forEach((star, index2) => {
+    // Add a mouseover event listener to each star
+    star.addEventListener('mouseover', () => {
+        highlightStars(index1);
+    });
 
-            // Add 'gold' class to stars up to and including the clicked star index,
-            // and remove 'gold' class from stars after the clicked star index
-            index1 >= index2 ? star.classList.add('gold') : star.classList.remove('gold');
-
-            // Calculate the rating based on the clicked star index
-            rating = index1 + 1;
-            console.log(rating);
-
-        });
+    // Add a mouseout event listener to each star
+    star.addEventListener('mouseout', () => {
+        removeHighlights();
+        updateGoldStars(rating - 1);
     });
 });
+
+// Function to update the rating
+function updateRating(clickedRating) {
+    rating = clickedRating;
+    console.log(rating);
+}
+
+// Function to highlight stars up to a given index
+function highlightStars(index) {
+    stars.forEach((star, index2) => {
+        if (index2 <= index) {
+            star.classList.add('gold');
+        }
+    });
+}
+
+// Function to remove all highlighted stars
+function removeHighlights() {
+    stars.forEach(star => {
+        star.classList.remove('gold');
+    });
+}
+
+// Function to update gold stars up to a given index
+function updateGoldStars(index) {
+    stars.forEach((star, index2) => {
+        if (index2 <= index) {
+            star.classList.add('gold');
+        } else {
+            star.classList.remove('gold');
+        }
+    });
+}
